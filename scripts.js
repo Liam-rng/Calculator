fd69c3738dff60072052294307a26f650668b709
tenso <liam.scott.7798@gmail.com>
2022-07-10 12:22:16 -0400

    Add div to show typed numbers, create operate function

--- scripts.js
+++ scripts.js
@@ -10,3 +10,18 @@ function division(arg1, arg2){
 function multiplication(arg1, arg2){
     return arg1 * arg2;
 }
+
+function operate(operator, arg1, arg2){
+    switch(operator) {
+        case "+": 
+            return addition(arg1, arg2);
+        case "-":
+            return subtraction(arg1, arg2);
+        case "/":
+            return division(arg1,arg2);
+        case "*": 
+            return multiplication(arg1, arg2);
+        default:
+            return "Operator not found.";
+    }
+}
