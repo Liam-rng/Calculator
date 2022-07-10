700961e1d27325ac32eab5c335fe6271bc84404d
tenso <liam.scott.7798@gmail.com>
2022-07-10 11:56:31 -0400

    Add the 4 basic arithmetic functions.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function addition(arg1, arg2){
+    return arg1 + arg2;
+}
+function subtraction(arg1, arg2){
+    return arg1 - arg2;
+}
+function division(arg1, arg2){
+    return arg1 / arg2;
+}
+function multiplication(arg1, arg2){
+    return arg1 * arg2;
+}
